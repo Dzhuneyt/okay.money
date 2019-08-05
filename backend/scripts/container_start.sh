3d4ec6aa891812dc96a64202c1b653e0d675ce8c
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-08-05 17:19:49 +0300

    feat: Execute migrations on AWS ElasticBeanstack launch time

---
+++ backend/scripts/container_start.sh
@@ -0,0 +1,11 @@
+##!/usr/bin/env sh
+
+file="/host_tmp/is_leader"
+if [ -f "$file" ]
+then
+  echo AWS BeanStalk environment update detected. Run migrations
+else
+  echo Not an AWS BeanStalk app. Running regular Apache
+fi
+
+apache2-foreground
--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,3 +1,4 @@
-
 timestamp=$(date +%s)
+
+docker system prune -f
 TAG=$timestamp docker-compose up --build
