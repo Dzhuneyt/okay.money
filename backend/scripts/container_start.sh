7dcf5abe60f9018968eeedb31d4b1f0be06957f6
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-08-07 14:35:20 +0300

    chore: Include additional subnet to Terraform stack

--- backend/scripts/container_start.sh
+++ backend/scripts/container_start.sh
@@ -1,8 +1,7 @@
 ##!/usr/bin/env sh
 
 file="/host_tmp/is_leader"
-if [ -f "$file" ]
-then
+if [ -f "$file" ]; then
   echo AWS BeanStalk environment update detected. Run migrations
 else
   echo Not an AWS BeanStalk app. Running regular Apache
