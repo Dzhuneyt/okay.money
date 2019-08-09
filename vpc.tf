e8b931b02e54577eadae754a6282a501994de591
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-08-09 15:33:52 +0300

    chore: Proper naming for Beanstalk resources

--- vpc.tf
+++ vpc.tf
@@ -4,6 +4,8 @@ locals {
     "10.0.1.0/24",
     "10.0.2.0/24",
     "10.0.3.0/24"]
+
+  stack_name = "Personal-Finance-Beanstalk"
 }
 
 provider "aws" {
@@ -17,6 +19,9 @@ data "aws_region" "current" {
 resource "aws_vpc" "main" {
   cidr_block = local.cidr_block
   enable_dns_hostnames = true
+  tags = {
+    Name = local.stack_name
+  }
 }
 
 # Declare the data source
@@ -29,6 +34,10 @@ resource "aws_subnet" "public_subnets" {
   count = length(local.public_subnets)
   cidr_block = local.public_subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${local.stack_name}-${count.index}"
+  }
 }
 
 # Set the default vpc
@@ -49,6 +58,10 @@ data "aws_subnet_ids" "all_subnets" {
 # Create the internet gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = local.stack_name
+  }
 }
 
 # Create route table that allows ingress traffic from everywhere
@@ -58,6 +71,10 @@ resource "aws_route_table" "route_table" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = local.stack_name
   }
 }
 
