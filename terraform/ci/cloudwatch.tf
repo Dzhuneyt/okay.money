325d3e75aa9e52a75de3982282fcc666b014d9ca
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-09-02 16:05:27 +0300

    chore: Make scripts executable refactor: Abstract away some CloudWatch code into a module

--- terraform/ci/cloudwatch.tf
+++ terraform/ci/cloudwatch.tf
@@ -1,88 +1,17 @@
 # Store build logs here and keep them for 14 days
 resource "aws_cloudwatch_log_group" "codebuild" {
-  name              = "${var.tag}-codebuild"
+  name = "${var.tag}-codebuild"
   retention_in_days = 14
 }
 
-
-# Listen for activity on the CodeCommit repo and trigger the CodePipeline
-locals {
-  git_branch       = "develop"
+module "cloudwatch_triggers_codepipeline" {
+  source = "github.com/Dzhuneyt/terraform-cloudwatch-trigger-codepipeline?ref=0.0.1"
+  branch_to_monitor = "develop"
+  codecommit_repo_arn = data.aws_codecommit_repository.test.arn
   codepipeline_arn = aws_codepipeline.codepipeline_develop.arn
-  repo_arn         = data.aws_codecommit_repository.test.arn
-}
-resource "aws_cloudwatch_event_rule" "codecommit_activity" {
-  name        = "${var.tag}-codecommit-activity"
-  description = "Detect commits to CodeCommit repo of ${var.tag}"
+  tag = var.tag
 
-  event_pattern = <<PATTERN
-{
-  "source": [ "aws.codecommit" ],
-  "detail-type": [ "CodeCommit Repository State Change" ],
-  "resources": [ "${local.repo_arn}" ],
-  "detail": {
-     "event": [
-       "referenceCreated",
-       "referenceUpdated"
-      ],
-     "referenceType":["branch"],
-     "referenceName": ["${local.git_branch}"]
+  providers = {
+    aws = aws
   }
-}
-PATTERN
-}
-
-resource "aws_cloudwatch_event_target" "cloudwatch_triggers_pipeline" {
-  target_id = "${var.tag}-commits-trigger-pipeline"
-  rule      = aws_cloudwatch_event_rule.codecommit_activity.name
-  arn       = local.codepipeline_arn
-  role_arn  = aws_iam_role.cloudwatch_ci_role.arn
-}
-
-# Allows the CloudWatch event to assume roles
-resource "aws_iam_role" "cloudwatch_ci_role" {
-  name = "${var.tag}-cloudwatch-ci-role"
-
-  assume_role_policy = <<DOC
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "events.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-DOC
-}
-data "aws_iam_policy_document" "cloudwatch_ci_iam_policy" {
-  statement {
-    actions = [
-      "iam:PassRole"
-    ]
-    resources = [
-      "*"
-    ]
-  }
-  statement {
-    # Allow CloudWatch to start the Pipeline
-    actions = [
-      "codepipeline:StartPipelineExecution"
-    ]
-    resources = [
-      local.codepipeline_arn
-    ]
-  }
-}
-resource "aws_iam_policy" "cloudwatch_ci_iam_policy" {
-  name_prefix = "${var.tag}-cloudwatch-ci-"
-  policy      = data.aws_iam_policy_document.cloudwatch_ci_iam_policy.json
-}
-resource "aws_iam_role_policy_attachment" "cloudwatch_ci_iam" {
-  policy_arn = aws_iam_policy.cloudwatch_ci_iam_policy.arn
-  role       = aws_iam_role.cloudwatch_ci_role.name
 }
