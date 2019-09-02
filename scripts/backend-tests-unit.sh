6d50e3ce453158f13e27c66be9dec07acb99661b
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-09-02 21:49:38 +0300

    feat(ci): Introducing new CodeBuild instance for tests

--- scripts/backend-tests-unit.sh
+++ scripts/backend-tests-unit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.yml -f docker-compose.local.override.yml up --build backend_unit_tests_runner
+docker-compose -f docker-compose.yml -f docker-compose.local.override.yml up --no-color --build backend_unit_tests_runner
