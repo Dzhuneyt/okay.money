caf6f08542d7e86243c5633d1ff5a4db2bd7c3bd
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-09-02 21:47:22 +0300

    feat(ci): Introducing new CodeBuild instance for tests

--- scripts/backend-tests-functional.sh
+++ scripts/backend-tests-functional.sh
@@ -4,5 +4,3 @@
 set -e
 
 docker-compose -f docker-compose.yml -f docker-compose.local.override.yml up --no-color --exit-code-from backend_functional_tests_runner backend_functional_tests_runner
-
-exit $?
