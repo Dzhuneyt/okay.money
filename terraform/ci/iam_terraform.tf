0e0748e563a84476c45b583ad997a60afa74c36d
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-09-10 16:28:26 +0300

    refactor: Move everything to a single module for easier maintenance

--- terraform/ci/iam_terraform.tf
+++ terraform/ci/iam_terraform.tf
@@ -162,7 +162,8 @@ data "aws_iam_policy_document" "terraform_policy" {
 
   statement {
     actions = [
-      "events:Describe*"
+      "events:Describe*",
+      "events:List*",
     ]
     resources = [
       "*"
