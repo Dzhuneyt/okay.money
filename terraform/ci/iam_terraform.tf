9077861c5081651037e7955fe08bafbee59b50ec
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-09-10 18:47:08 +0300

    refactor: Move everything to a single module for easier maintenance

--- terraform/ci/iam_terraform.tf
+++ terraform/ci/iam_terraform.tf
@@ -91,6 +91,7 @@ data "aws_iam_policy_document" "terraform_policy" {
       "ecs:Describe*",
       "ecs:CreateCluster",
       "ecs:RegisterTaskDefinition",
+      "ecs:DeregisterTaskDefinition",
     ]
     resources = [
     "*"]
