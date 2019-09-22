a6b7041361a7d723ac3a4c841d52a54cb6c7c51d
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-09-22 18:52:26 +0300

    chore: Debug CI

--- scripts/terraform_create_ci.sh
+++ scripts/terraform_create_ci.sh
@@ -10,19 +10,18 @@ BASEDIR=$(pwd)
 #  cd "$BASEDIR"/terraform/vpc && terraform init && terraform apply -auto-approve &&
 #  echo VPC provisioning finished
 
-VPC_ID=$(cd $BASEDIR/terraform/vpc && terraform output vpc_id)
-PRIVATE_SUBNETS=$(cd $BASEDIR/terraform/vpc && terraform output private_subnets | tr -d '\n')
-PUBLIC_SUBNETS=$(cd $BASEDIR/terraform/vpc && terraform output public_subnets | tr -d '\n')
+#VPC_ID=$(cd $BASEDIR/terraform/vpc && terraform output vpc_id)
+#PRIVATE_SUBNETS=$(cd $BASEDIR/terraform/vpc && terraform output private_subnets | tr -d '\n')
+#PUBLIC_SUBNETS=$(cd $BASEDIR/terraform/vpc && terraform output public_subnets | tr -d '\n')
+
+# Initialize base directories
 
 tmpfile=$(mktemp)
 # shellcheck disable=SC2129
-echo private_subnets="${PRIVATE_SUBNETS}" >>"$tmpfile"
-echo public_subnets="${PUBLIC_SUBNETS}" >>"$tmpfile"
-echo vpc_id="\"${VPC_ID}\"" >>"$tmpfile"
 
 echo "Creating CodePipeline & CodeBuild..."
 cd "$BASEDIR"/terraform/ci &&
-  terraform init > /dev/null &&
+  terraform init &&
   terraform apply -var-file="$tmpfile" -auto-approve -refresh=true
 
 echo "CodePipeline & CodeBuild created"
