a73714de28b34bffde2d2c343d2081f2a9bb6cdb
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-09-22 15:55:22 +0300

    feat: ECR created and managed by Terraform - uses remote state as well

--- scripts/docker-compose-push.sh
+++ scripts/docker-compose-push.sh
@@ -1,8 +1,15 @@
-##!/usr/bin/env sh
+#!/usr/bin/env bash
 
 timestamp=$1
+if [ -z "$1" ]
+  then
+    timestamp=$(date -u +"%F-%H-%M-UTC")
+fi
 
-$(aws ecr get-login --region eu-west-1 --no-include-email)
-TAG=$timestamp docker-compose pull
-TAG=$timestamp docker-compose build
-TAG=$timestamp docker-compose push
+REGION="eu-west-1"
+ECR_PREFIX="216987438199.dkr.ecr.${REGION}.amazonaws.com/finance"
+
+$(aws ecr get-login --region ${REGION} --no-include-email)
+ECR_PREFIX=${ECR_PREFIX} TAG=${timestamp} docker-compose pull
+ECR_PREFIX=${ECR_PREFIX} TAG=${timestamp} docker-compose build --parallel
+ECR_PREFIX=${ECR_PREFIX} TAG=${timestamp} docker-compose push
---
+++ terraform/ecr/README.md
@@ -0,0 +1,1 @@
+Execute terraform apply here one time to create the necessary ECR repos to exist
