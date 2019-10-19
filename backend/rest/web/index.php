bb7f80d14f4240b595d07fb551497dce5afeff78
dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-10-19 21:56:02 +0300

    fix: Backend API failure if starting clean chore: Increment Node version of FE builder from 10 to 11

--- backend/rest/web/index.php
+++ backend/rest/web/index.php
@@ -19,7 +19,7 @@ require(__DIR__ . '/../config/bootstrap.php');
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../common/config/main.php'),
     require(__DIR__ . '/../config/main.php'),
-    require(__DIR__ . '/../config/main-local.php')
+    requireIfExists(__DIR__ . '/../config/main-local.php')
 );
 
 try {
