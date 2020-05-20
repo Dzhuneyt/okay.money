71e5af068b34b5aca6cdf58692e9ed49aead7698
Dzhuneyt Ahmed <dzhuneyt@dzhuneyt.com>
2020-05-20 11:24:34 +0300

    feat: Transaction create

---
+++ cdk/lambdas/interfaces/ITransaction.ts
@@ -0,0 +1,12 @@
+interface ITransaction {
+    id?: string;
+    description: string,
+    sum: number,
+    type: "expense" | "income",
+    account_id: string,
+    category_id: string,
+
+    // Since the input is unpredictable, allow any other values
+    // to "checked" by the code below without causing compilation errors
+    [key: string]: any,
+}
--- cdk/lambdas/transaction-create.ts
+++ cdk/lambdas/transaction-create.ts
@@ -4,10 +4,10 @@ import {IEvent} from './interfaces/IEvent';
 import DynamoDB = require('aws-sdk/clients/dynamodb');
 import {v4 as uuidv4} from 'uuid';
 
-interface Input {
-    title: string,
-    account_id: string,
-    category_id: string,
+interface Input extends ITransaction {
+    // Since the input is unpredictable, allow any other values
+    // to "checked" by the code below without causing compilation errors
+    [key: string]: any,
 }
 
 export const handler = async (event: IEvent, context: any) => {
@@ -17,38 +17,38 @@ export const handler = async (event: IEvent, context: any) => {
         const userId = event.requestContext.authorizer.sub
         const params: Input = JSON.parse(event.body || '{}');
 
-        if (!params.title) {
-            throw new Error(`Invalid parameter "title"`);
-        }
-
-        if (!params.category_id) {
-            throw new Error(`Invalid parameter "category_id"`);
-        }
-        if (!params.account_id) {
-            throw new Error(`Invalid parameter "account_id"`);
-        }
+        [
+            'sum',
+            'type',
+            'category_id',
+            'account_id'
+        ].forEach((value: string | number) => {
+            if (!params[value]) {
+                throw new Error(`Invalid parameter "${value}"`);
+            }
+        });
 
         const tableName = process.env.TABLE_NAME as string;
         const dynamodb = new AWS.DynamoDB();
         const uuid = uuidv4();
-        const account: IAccount = {
+        const obj: ITransaction = {
+            ...params,
             id: uuid,
-            author_id: userId,
-            title: params.title,
+            author_id: userId
         };
-        const result = await dynamodb.putItem({
+        const putItem = await dynamodb.putItem({
             TableName: tableName,
-            Item: DynamoDB.Converter.marshall(account),
+            Item: DynamoDB.Converter.marshall(obj),
         }).promise();
 
-        if (result.$response.error) {
+        if (putItem.$response.error) {
             return {
                 statusCode: 500,
-                body: result.$response.error.message,
+                body: putItem.$response.error.message,
             }
         }
 
-        const item = await dynamodb.getItem({
+        const getItem = await dynamodb.getItem({
             Key: {
                 id: {
                     S: uuid
@@ -58,7 +58,7 @@ export const handler = async (event: IEvent, context: any) => {
         }).promise();
         return {
             statusCode: 200,
-            body: JSON.stringify(DynamoDB.Converter.unmarshall(item.Item!)),
+            body: JSON.stringify(DynamoDB.Converter.unmarshall(getItem.Item!)),
         }
     } catch (e) {
         console.log(e);
