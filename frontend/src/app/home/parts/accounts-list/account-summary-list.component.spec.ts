17b223fd2d5a6941ef027b24be247359fd2b3da0
Dzhuneyt Ahmed <dzhuneyt@dzhuneyt.com>
2020-12-19 23:15:31 +0200

    @angular/core migration - migration-v11-wait-for-async
    `async` to `waitForAsync` migration. The `async` testing function has been renamed to `waitForAsync` to avoid confusion with the native `async` keyword.

--- frontend/src/app/home/parts/accounts-list/account-summary-list.component.spec.ts
+++ frontend/src/app/home/parts/accounts-list/account-summary-list.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {AccountSummaryListComponent} from 'src/app/home/parts/accounts-list/account-summary-list.component';
 
@@ -6,7 +6,7 @@ describe('AccountsListComponent', () => {
     let component: AccountSummaryListComponent;
     let fixture: ComponentFixture<AccountSummaryListComponent>;
 
-    beforeEach(async(() => {
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [AccountSummaryListComponent]
         })
--- frontend/src/app/stats-by-category/stats-by-category.component.spec.ts
+++ frontend/src/app/stats-by-category/stats-by-category.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {StatsByCategoryComponent} from './stats-by-category.component';
 
@@ -6,7 +6,7 @@ describe('StatsByCategoryComponent', () => {
   let component: StatsByCategoryComponent;
   let fixture: ComponentFixture<StatsByCategoryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [StatsByCategoryComponent]
     })
