5825cabbb5d2199f410430dfe375b5b32db2c900
Dzhuneyt Ahmed <dzhuneyt@dzhuneyt.com>
2021-01-08 11:16:49 +0200

    feat: Attach the CloudFront to Route53

--- cdk/lib/stacks/FrontendStack/FrontendStack.ts
+++ cdk/lib/stacks/FrontendStack/FrontendStack.ts
@@ -1,17 +1,28 @@
 import {Annotations, CfnOutput, Construct, RemovalPolicy, Stack, StackProps} from "@aws-cdk/core";
 import {Bucket, IBucket} from "@aws-cdk/aws-s3";
-import {Distribution, OriginAccessIdentity} from "@aws-cdk/aws-cloudfront";
+import {
+    CachePolicy,
+    Distribution,
+    IDistribution,
+    OriginAccessIdentity,
+    PriceClass,
+    ViewerProtocolPolicy
+} from "@aws-cdk/aws-cloudfront";
 import * as origins from '@aws-cdk/aws-cloudfront-origins';
 import {BucketDeployment, Source} from "@aws-cdk/aws-s3-deployment";
-import {HostedZone, IHostedZone} from "@aws-cdk/aws-route53";
+import {ARecord, HostedZone, IHostedZone, RecordTarget} from "@aws-cdk/aws-route53";
+import {Certificate, ICertificate} from "@aws-cdk/aws-certificatemanager";
+import {CloudFrontTarget} from "@aws-cdk/aws-route53-targets";
 
 export class FrontendStack extends Stack {
     private readonly bucket: IBucket;
+    private distribution: IDistribution;
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props);
 
         const hostedZone = this.getHostedZone();
+        const certificate = this.getCertificate();
 
         this.bucket = new Bucket(this, 'Bucket', {
             removalPolicy: RemovalPolicy.DESTROY,
@@ -31,11 +42,14 @@ export class FrontendStack extends Stack {
         const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
         this.bucket.grantRead(originAccessIdentity);
 
-        const distribution = new Distribution(this, 'Distribution', {
+        this.distribution = new Distribution(this, 'Distribution', {
             defaultBehavior: {
                 origin: new origins.S3Origin(this.bucket, {
                     originAccessIdentity,
-                })
+                }),
+                viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                compress: true,
+                cachePolicy: CachePolicy.CACHING_OPTIMIZED,
             },
             // When "/" is visited, assume it's index.html
             defaultRootObject: 'index.html',
@@ -48,16 +62,35 @@ export class FrontendStack extends Stack {
                 },
             ],
             domainNames: hostedZone ? [hostedZone.zoneName] : undefined,
+            priceClass: PriceClass.PRICE_CLASS_100,
+            certificate: certificate,
         });
 
-        new CfnOutput(this, 'distribution-output', {
-            value: distribution.distributionDomainName,
-        })
+        // Attach the CloudFront as an Alias target to the Route53 HostedZone
+        if (process.env.ENV_NAME === 'master' && hostedZone) {
+            new ARecord(this, 'ARecord', {
+                zone: hostedZone,
+                target: RecordTarget.fromAlias(new CloudFrontTarget(this.distribution)),
+            });
+        }
+
+        // Stack Outputs
+        new CfnOutput(this, 'cloudfront-url', {value: this.distribution.distributionDomainName,});
+        if (hostedZone) {
+            new CfnOutput(this, 'public-url', {value: hostedZone.zoneName});
+        }
     }
 
-    getHostedZone(): IHostedZone | void {
-        if (process.env.ENV_NAME === 'master') {
-            return HostedZone.fromLookup(this, 'HostedZone', {domainName: 'okay.money'});
-        }
+    getHostedZone(): IHostedZone | undefined {
+        return process.env.ENV_NAME === 'master'
+            ? HostedZone.fromLookup(this, 'HostedZone', {domainName: 'okay.money'})
+            : undefined;
+    }
+
+    getCertificate(): ICertificate | undefined {
+        // ACM to be used in CloudFront must be in us-east-1 regions
+        return process.env.ENV_NAME === 'master'
+            ? Certificate.fromCertificateArn(this, 'Certificate', 'arn:aws:acm:us-east-1:347315207830:certificate/a855b6f4-8017-4a17-8daa-d56848a1a7af')
+            : undefined;
     }
 }
