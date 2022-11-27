3380d74b98afbe2831417c68628642e2d93850fe
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-11-28 00:56:41 +0200

    ci: Develop deployment through GitHub Actions

--- cdk/lib/stacks/GithubOidcProviderStack/GithubOidcProviderStack.ts
+++ cdk/lib/stacks/GithubOidcProviderStack/GithubOidcProviderStack.ts
@@ -1,6 +1,7 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
 import {GithubActionsIdentityProvider, GithubActionsRole} from "aws-cdk-github-oidc";
+import {PolicyStatement} from "aws-cdk-lib/aws-iam";
 
 export class GithubOidcProviderStack extends Stack {
 
@@ -8,11 +9,15 @@ export class GithubOidcProviderStack extends Stack {
         super(scope, id, props);
         const provider = new GithubActionsIdentityProvider(this, 'GithubProvider');
 
-        new GithubActionsRole(this, 'GithubActionsRole', {
+        const role = new GithubActionsRole(this, 'GithubActionsRole', {
             provider: provider,           // reference into the OIDC provider
             owner: 'Dzhuneyt',            // your repository owner (organization or user) name
             repo: 'okay.money',            // your repository name (without the owner name)
             roleName: 'github-actions',
         });
+        role.addToPolicy(new PolicyStatement({
+            actions: ['sts:AssumeRole'],
+            resources: ["arn:aws:iam::*:role/cdk-*"],
+        }))
     }
 }
